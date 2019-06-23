0aba024504c10a0bd88d0a05df00ae356ca992e2
image-search-gallery <image.search.gallery@gmail.com>
2019-06-23 15:51:01 +0200

    Search listener

--- app/src/main/java/com/project/gallery/search/view/GallerySearchView.kt
+++ app/src/main/java/com/project/gallery/search/view/GallerySearchView.kt
@@ -1,22 +1,25 @@
 package com.project.gallery.search.view
 
 import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.FrameLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.*
+import androidx.recyclerview.widget.RecyclerView.OnScrollListener
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.project.gallery.R
 import com.project.gallery.image.HttpsBitmapUrlLoader
 import com.project.gallery.image.ImageLoader
 import com.project.gallery.search.view.GallerySearchPresenter.*
-import com.project.gallery.search.view.GallerySearchPresenter.State
 import com.project.gallery.search.view.GallerySearchPresenter.State.*
 import kotlinx.android.synthetic.main.gallery_search_grid_item.view.*
-import kotlinx.android.synthetic.main.gallery_search_grid_view.view.*
+import kotlinx.android.synthetic.main.gallery_search_view.view.*
 import java.util.concurrent.Executors
 
 class GallerySearchView(context: Context, attributeSet: AttributeSet) : GallerySearchPresenter,
@@ -36,7 +39,7 @@ class GallerySearchView(context: Context, attributeSet: AttributeSet) : GalleryS
     init {
         LayoutInflater
             .from(context)
-            .inflate(R.layout.gallery_search_grid_view, this, true)
+            .inflate(R.layout.gallery_search_view, this, true)
 
         (gallerySearchGrid as RecyclerView).apply {
             layoutManager = GridLayoutManager(context, GRID_COLUMN_COUNT)
@@ -54,6 +57,22 @@ class GallerySearchView(context: Context, attributeSet: AttributeSet) : GalleryS
                 }
             })
         }
+
+        (searchField as EditText).addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                // no-op
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // no-op
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s != null && s.isNotEmpty()) {
+                    viewEventsListener?.search(s.toString())
+                }
+            }
+        })
     }
 
     override fun updateState(state: State) {
@@ -107,7 +126,7 @@ class GallerySearchView(context: Context, attributeSet: AttributeSet) : GalleryS
         viewEventsListener = listener
     }
 
-    private inner class ImageAdapter(context: Context) : Adapter<ViewHolder>() {
+    private inner class ImageAdapter(context: Context) : RecyclerView.Adapter<ViewHolder>() {
 
         private val READY_ITEM_TYPE = 0
         private val LOADING_ITEM_TYPE = 1
