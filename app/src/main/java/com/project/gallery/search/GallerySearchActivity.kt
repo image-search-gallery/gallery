8ef5a5a802fb183a43b2f4175e81537ce5492033
image-search-gallery <image.search.gallery@gmail.com>
2019-06-23 14:00:43 +0200

    Icon pics

--- app/src/main/java/com/project/gallery/search/GallerySearchActivity.kt
+++ app/src/main/java/com/project/gallery/search/GallerySearchActivity.kt
@@ -1,17 +1,26 @@
 package com.project.gallery.search
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import com.project.gallery.R
+import com.project.gallery.search.data.repository.InMemoryImageRepository
+import com.project.gallery.search.domain.GallerySearchInteractor
 import kotlinx.android.synthetic.main.gallery_search_activity.*
 
 class GallerySearchActivity : AppCompatActivity() {
 
+    lateinit var interactor: GallerySearchInteractor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.gallery_search_activity)
+
+        interactor = GallerySearchInteractor(InMemoryImageRepository(), gallerySearchView)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        interactor.start()
+        interactor.search(InMemoryImageRepository.KITTENS_KEYWORD)
     }
 }
