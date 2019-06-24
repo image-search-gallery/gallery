7c717bc504edd41875a7141e1c1bb10430458ff7
anastasia-zaitsewa <anastasia.zaitsewa@gmail.com>
2019-06-24 20:57:59 +0200

    Throttle inteval checks

--- app/src/main/java/com/project/gallery/utils/Throttler.kt
+++ app/src/main/java/com/project/gallery/utils/Throttler.kt
@@ -1,12 +1,24 @@
 package com.project.gallery.utils
 
+import java.lang.IllegalArgumentException
 import java.util.*
 
 class Throttler(private val throttleByMilliseconds: Long){
     private val timer = Timer()
     private var timerTask: TimerTask? = null
 
+    init {
+        if(throttleByMilliseconds < 0){
+            throw IllegalArgumentException("Throttle interval must not be negative.")
+        }
+    }
+
     fun submit(task: () -> Unit) {
+        if(throttleByMilliseconds == 0L){
+            task()
+            return
+        }
+
         timerTask?.cancel()
 
         timerTask = object : TimerTask() {
